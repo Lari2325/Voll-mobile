9dc38c36ed0be3bed56c13d13e40ed078f4e2a5e
Lari2325 <b35.larissa@gmail.com>
2024-06-24 10:27:05 -0300

    Descrição das mudanças

--- src/Login.tsx
+++ src/Login.tsx
@@ -10,47 +10,41 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import jwtDecode from 'jwt-decode';
 
 export default function Login({ navigation } : any) {
-
   const [email, setEmail] = useState('')
   const [senha, setSenha] = useState('')
   const [carregando, setCarregando] = useState(true)
   const toast = useToast()
 
   useEffect(() => {
-      async function verificarLogin(){
+    async function verificarLogin(){
       const token = await AsyncStorage.getItem('token')
-      if (token) {
+      if(token){
         navigation.replace('Tabs')
       }
       setCarregando(false)
     }
     verificarLogin()
-  }, [])
-  
+  },[])
+
   async function login(){
     const resultado = await fazerLogin(email, senha)
-  
     if(resultado){
-
       const { token } = resultado
-
       AsyncStorage.setItem('token', token)
 
       const tokenDecodificado = jwtDecode(token) as any
-      const pacienteId = jwtDecode(token).id
-
-      AsyncStorage.setItem('pacienteId', pacienteId)
-
+      const pacienteId = tokenDecodificado.id
+      AsyncStorage.setItem('pacientId', pacienteId)
       navigation.replace('Tabs')
-    } else {
+    }
+    else{
       toast.show({
-        title: 'Falha ao fazer login',
-        description: 'Verifique seus dados e tente novamente',
-        background: 'red.500'
+        title: "Erro no login",
+        description: "O email ou senha não conferem",
+        backgroundColor: "red.500"
       })
     }
   }
-
 
   if(carregando){
     return null
@@ -58,7 +52,7 @@ export default function Login({ navigation } : any) {
 
   return (
     <VStack flex={1} alignItems="center" justifyContent="center" p={5}>
-      <Image source={Logo} alt="Logo Voll" alignSelf="center" />
+      <Image source={Logo} alt="Logo Voll" />
 
       <Titulo>
         Faça login em sua conta
@@ -77,7 +71,7 @@ export default function Login({ navigation } : any) {
           onChangeText={setSenha}
           secureTextEntry
         />
-      </Box> 
+      </Box>
       <Botao onPress={login}>Entrar</Botao>
 
       <Link href='https://www.alura.com.br' mt={2}>
