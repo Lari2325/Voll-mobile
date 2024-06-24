f1a8764743f4868a5225e6dc9b999cc7a612cb5a
Lari2325 <b35.larissa@gmail.com>
2024-06-24 09:03:48 -0300

    atualizando pasta

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { NativeBaseProvider, StatusBar } from 'native-base';
 import { TEMAS } from './src/estilos/temas';
 
 export default function App() {
+
   return (
     <NativeBaseProvider theme={TEMAS}>
       <StatusBar backgroundColor={TEMAS.colors.blue[800]} />
